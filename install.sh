b8e187e4191abb3e763a370ac4b2d85a82a4abb9
Wiley-WL <87199429+Wiley-WL@users.noreply.github.com>
2021-08-24 16:26:41 +0800

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# SUBSCRIBE
-sudo apt-get update
-sudo apt-get install figlet
-sudo apt-get install pv
-sudo apt-get install cgpt
-sudo bash chromeos-install.sh -src samus_recovery.bin -dst /dev/sda
-
-
